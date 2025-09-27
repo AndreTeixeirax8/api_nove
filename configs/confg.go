7afc5ad50649f7eb909309ae05371f92a04539df
André Teixeira <andre-x8@hotmail.com>
2025-09-27 10:33:06 -0300

    adiciona funcao de leitura das variaveis de ambiente

--- configs/confg.go
+++ configs/confg.go
@@ -1,5 +1,14 @@
 package configs
 
+import (
+	"log"
+
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
 type conf struct {
 	DBDriver      string
 	DBHost        string
@@ -10,4 +19,31 @@ type conf struct {
 	WebServerPort string
 	JWTSecret     string
 	JwtExperesIn  int //Tempo de validação do token
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+// Fnção para carregar as configurações
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Println("❌ Erro ao ler as configurações do viper.ReadConfig")
+		panic(err)
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.Println("❌ Erro ao usar o unmarshal no cfg")
+		panic(err)
+	}
+
+	//Cria uma instancia para poder gerar o token jwt
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg, err
+
 }
