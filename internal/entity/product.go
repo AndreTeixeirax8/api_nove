6f8b3267b924b48a86489551bb8a1716b92ba20a
André Teixeira <andre-x8@hotmail.com>
2025-09-30 21:15:58 -0300

    adiciona entidade de produto

---
+++ internal/entity/product.go
@@ -0,0 +1,59 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/api_nove/pkg/entity"
+)
+
+var (
+	ErrIDIsRequired    = errors.New("ID é requerido")
+	ErrInvalidID       = errors.New("ID é invalido")
+	ErrNameIsRequired  = errors.New("Nome é obrigatorio")
+	ErrPriceIsRequerid = errors.New("Preço é obrigatorio")
+	ErrInvalidPrice    = errors.New("Preço invalido")
+)
+
+type Product struct {
+	ID        entity.ID `json:"id"`
+	Name      string    `json:"name"`
+	Price     int       `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewProduct(name string, price int) (*Product, error) {
+	product := &Product{
+		ID:        entity.NewIdD(),
+		Name:      name,
+		Price:     price,
+		CreatedAt: time.Now(),
+	}
+	err := product.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+func (p *Product) Validate() error {
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequerid
+	}
+
+	if p.Price < 0 {
+		return ErrInvalidPrice
+	}
+	return nil
+}
