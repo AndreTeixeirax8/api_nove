f27a7fc3443d0f39b54745c72f872e3cb3ee6251
André Teixeira <andre-x8@hotmail.com>
2025-10-03 23:29:32 -0300

    adiciona user db

--- internal/infra/db/user_db.go
+++ internal/infra/db/user_db.go
@@ -1,6 +1,11 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"log"
+
+	"github.com/api_nove/internal/entity"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	DB *gorm.DB
@@ -8,4 +13,17 @@ type User struct {
 
 func NewProduct(db *gorm.DB) *User {
 	return &User{DB: db}
+}
+
+func (u *User) Create(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	if err := u.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		log.Println("❌ Erro ao buscar e-mail")
+		return nil, err
+	}
+	return &user, nil
 }
