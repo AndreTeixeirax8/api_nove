f676f7426b75f6ae706cf163fba4d9adcdaa62d2
André Teixeira <andre-x8@hotmail.com>
2025-10-15 21:36:30 -0300

    ajuste no handler jwt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -39,19 +40,39 @@ func main() {
 	productHandler := handlers.NewProductHandler(productDB)
 
 	userDb := database.NewUser(db)
-	userHandler := handlers.NewUserHandler(userDb, config.TokenAuth, config.JwtExpiresIn)
+	userHandler := handlers.NewUserHandler(userDb)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Get("/products", productHandler.GetAllProducts)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	//r.Use(LogRequest)
+	r.Use(middleware.Recoverer) // se der erro ele não deixa aplicação travada
+	r.Use(middleware.WithValue("jwt", config.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", config.JwtExpiresIn))
+
+	r.Route("/products", func(r chi.Router) {
+
+		//usar token de autenticacao
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetAllProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+
+	})
 
 	r.Post("/users", userHandler.Create)
 	r.Post("/users/generate_token", userHandler.GetJWT)
 
 	fmt.Printf("✅ Servidor rodando na porta %s\n", config.WebServerPort)
 	http.ListenAndServe(fmt.Sprintf(":%s", config.WebServerPort), r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Recebida requisição: %s %s\n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
